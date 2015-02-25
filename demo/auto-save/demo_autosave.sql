ba8fdc2537d93276faa0bf66f31395977aa82231
yoyonel <yoyonel@hotmail.com>
2015-02-25 09:52:22 +0100

    synchronisation for adding (sql, demo)

---
+++ demo/auto-save/demo_autosave.sql
@@ -0,0 +1,70 @@
+﻿-------------------------------
+--Rémi-C
+--Thales IGN 02/2015
+-------------------------------
+
+------
+-- this example create a table with 2 geometry columns
+-- one is a linestring, the other a polygon
+-- the aim is that when the linestring is changed, the polygon is automatically updated
+-- we use a trigger for that
+-- the same can be done for geometries in 2 or more different tables
+-- here for demonstration purpose, we compute poly as a random buffer of line
+
+/** Instruction : 
+
+  + execute this sql script. It will create a new table auto_save.line_to_poly_sync in your PostGIS database
+  + create an empty QGIS 2.6 project, load the 2 geometrical layers of auto_save.line_to_poly_sync.
+    - You have now 2 postgis layers in your qgis project
+  + Get the extension "Interactive Map Tracking"  from QGIS plug-in repository or this github : https://github.com/Remi-C/interactive_map_tracking
+  + Launch the extension by clicking on the icon ( T ), and enable it and auto-save by checking the comboboxes.
+
+  + Edit the line layer. you can insert, delete, update the geometry, translate, etc
+  + Each time you edit the line, the poly layer is automatically updated, and QGIS rendering refreshed
+  + The poly associated to the line you changed is updated (with a random buffer so you see that something changed) instantaneously !
+
+  + It still works without the plugin, but after each edit, you have to save it and refresh (for instance move camera, or zoom/dezoom)
+*/
+ 
+ --checking that postgis is installed
+ CREATE EXTENSION IF NOT EXISTS postgis ;
+ 
+ --creating a schema to put our tracking table
+ CREATE SCHEMA IF NOT EXISTS auto_save ;
+
+ --creating the minimal tracking table, other column can be added at will
+ DROP TABLE IF EXISTS auto_save.line_to_poly_sync ;
+ CREATE TABLE IF NOT EXISTS auto_save.line_to_poly_sync ( 
+	gid SERIAL PRIMARY KEY --mandatory
+	, line geometry(linestring, 4326) -- use your custom SRID here
+	, poly geometry(polygon,4326)  --use your custom SRID here 
+ ); 
+--creating index is not mandatory but will speed up all usual operations on those columns
+CREATE INDEX ON auto_save.line_to_poly_sync USING GIST(line) ; 
+CREATE INDEX ON auto_save.line_to_poly_sync USING GIST(poly) ; 
+
+--checking that the table was correctly created
+ SELECT *
+ FROM auto_save.line_to_poly_sync;  
+
+--creating a function that will be called by the trigger
+ CREATE OR REPLACE FUNCTION rc_buffer_line_on_insert_or_update(  )
+  RETURNS  trigger  AS
+$BODY$ 
+/** @brief : this trigger compute the buffer of line (random()/10) and save it at each change (insert/update)
+*/ 
+	DECLARE 
+	BEGIN   
+		NEW.poly = ST_Buffer(NEW.line,random()/10.0);
+		RETURN NEW ;
+	END ;
+	$BODY$
+ LANGUAGE plpgsql VOLATILE;
+  
+ --now associating the function to any UPDATE OR INSERT happening on auto_save.line_to_poly_sync table.
+DROP TRIGGER IF EXISTS rc_buffer_line_on_insert_or_update ON  auto_save.line_to_poly_sync; 
+CREATE TRIGGER rc_buffer_line_on_insert_or_update 
+BEFORE  UPDATE OR INSERT --here we define when the function should be called. No need to call it on DELETE ! 
+ON auto_save.line_to_poly_sync
+FOR ROW --for each changed row, the function will be called
+EXECUTE PROCEDURE rc_buffer_line_on_insert_or_update() ;
---
+++ sql_for_postgis/example_editing_duration_map.sql
@@ -0,0 +1,120 @@
+﻿---------------------------
+--Rémi Cura, 02/205, IGN THALES 
+---------------------------
+/** this example shows 2 ways to create a map of edition duration
+the first way is the most classical, and compute intersection of camera geometry then sum the time spent on each
+the second way create an hexagonal grid that covers the camera geometries.
+Then for each tile, we sum the time spent on it .
+Both example could be made to be interactively updated with trigger and few changes.
+This is especially true for the second example
+
+*/
+--LIMIT 100
+CREATE SCHEMA IF NOT EXISTS test ;  
+
+
+	DROP TABLE IF EXISTS  test.editing_duration_map;
+	CREATE TABLE test.editing_duration_map AS 
+		WITH camp_pos AS (--getting the tracking data, filtering to create map only for one user 
+			SELECT *
+			FROM tracking.camera_position  
+			WHERE user_id = 'Remi (192.168.1.2)'
+			ORDER BY w_time ASC, gid asc
+		)
+		,duration AS ( --computer the duration of each editing by taking the next camera position time and substract it from current camera tie
+			SELECT *,  lead(w_time ,1) over(ORDER BY w_time ASC , gid asc)  -w_time  as duration 
+			FROM camp_pos
+		)
+		, edit_time as ( --convert the time interval into seconds (decimal), to be able to work with it
+			SELECT gid, extract(EPOCH  from duration) as edit_time
+			FROM duration
+		)
+		, noded_geom AS (--take only the exterior ring of camera geometry, then union and node the geom.
+		--nodding means all intersection between lines will happen on a node
+			SELECT ST_Node(ST_Union(ST_ExteriorRing(geom))) as n_geom
+			FROM camp_pos
+		)
+		,polygonized_geom AS (--polygonize the linestrings, that is construct all possible minimal polygons from the linestring
+			SELECT st_polygonize(n_geom) as p_geom
+			FROM noded_geom
+		)
+		,part_of_camera AS (--extract each individual camera piece
+		SELECT row_number() over() as nid, dmp.geom as geom
+		FROM polygonized_geom, st_dump(p_geom) as dmp 
+		)
+		,mapping_partial_full AS ( --this is an inefficient way to compute this,and is only written here as example
+			--for each polygon piece, find in which original camera position it is, without having precision issue
+			SELECT DiSTINCT  cp.gid, po.nid,ST_Area(ST_Intersection(po.geom, cp.geom)) --distinct works with the order, to avoid precision issue
+			FROM part_of_camera AS po,  tracking.camera_position  AS cp
+			WHERE cp.user_id = 'Remi (192.168.1.2)'
+				AND ST_DWithin(po.geom, cp.geom,1) = TRUE --we don't use st_within because precision
+			ORDER BY   cp.gid, po.nid, ST_Area(ST_Intersection(po.geom, cp.geom)) DESC --when finding mutliple candidates take the best match
+		)
+		, grouped_time AS ( --now that each piece of polygon is linnked to an original camera and time, count how much time was spent on each piece of polygon
+			SELECT   nid, sum(et.edit_time) as tot_edit_time
+			FROM mapping_partial_full
+				NATURAL JOIN edit_time as et
+			GROUP BY nid
+		) --final result : taking previous result, but adding geometry for qgis display
+		SELECT nid, geom, tot_edit_time
+		FROM grouped_time
+			NATURAL join part_of_camera ; 
+
+
+
+------
+--Second version, compute also the time spend over editing, but on an hexagonal grid
+--NOTE : we separate the computing of the grid only for perfomance reason (namely, use of the index)
+--need : https://github.com/Remi-C/PPPP_utilities/blob/master/postgis/cdb_Hexagon.sql
+------
+	DROP TABLE IF EXISTS test.hexagonal_grid ;
+	CREATE TABLE test.hexagonal_grid AS
+		WITH camp_pos AS (--getting the tracking data, filtering to create map only for one user 
+			SELECT *
+			FROM tracking.camera_position  
+			WHERE user_id = 'Remi (192.168.1.2)'
+			ORDER BY w_time ASC, gid asc
+		)
+		, extent_of_tracking_data AS ( --simply getting the extent of all tracked data, that is the minimal rectangle (aligned with NS, EW) covering it
+			SELECT ST_Extent(geom) as geom
+			FROM camp_pos
+			--WHERE ST_DWithin(ST_SetSRID(ST_MakePoint(4818,18218),932011),geom, 800)
+			LIMIT 1 --security
+		)
+		--,generating_hexagonal_grid as (
+		--need : https://github.com/Remi-C/PPPP_utilities/blob/master/postgis/cdb_Hexagon.sql
+			SELECT row_number() over() AS nid, ST_SetSRID(hex,932011) as hex, 0.0 AS total_time_spent 
+			FROM extent_of_tracking_data, CDB_HexagonGrid(geom, 10) as hex; 
+
+		--some indexes, the geometric index is essential
+		CREATE INDEX ON test.hexagonal_grid USING GIST(hex); 
+		CREATE INDEX ON test.hexagonal_grid (total_time_spent); 
+		ALTER TABLE test.hexagonal_grid ADD primary key (nid) ; 
+			 
+		WITH camp_pos AS (--getting the tracking data, filtering to create map only for one user 
+			SELECT *
+			FROM tracking.camera_position  
+			WHERE user_id = 'Remi (192.168.1.2)'
+				AND ST_DWithin(ST_SetSRID(ST_MakePoint(4818,18218),932011),geom, 800)
+			ORDER BY w_time ASC, gid asc
+		)
+		,edit_time AS ( --computer the duration of each editing by taking the next camera position time and substract it from current camera tie
+			SELECT *,  LEAST(extract(EPOCH  from lead(w_time ,1) over(ORDER BY w_time ASC , gid asc)  -w_time)  ,100) as edit_time 
+			FROM camp_pos 
+		)     
+		,mapping_grid_to_edit_time AS (--edit the way it should be done
+			SELECT gh.nid, et.gid, et.edit_time
+			FROM test.hexagonal_grid  as gh, edit_time as et
+			WHERE ST_Intersects(gh.hex,et.geom )= TRUE
+				--AND edit_time BETWEEN 0 AND 10
+		)
+		, grouped_time AS ( --now that each piece of polygon is linnked to an original camera and time, count how much time was spent on each piece of polygon
+			SELECT   nid, sum(edit_time) as tot_edit_time
+			FROM mapping_grid_to_edit_time 
+			GROUP BY nid
+		) --finale, put the right information into hexagional table
+		UPDATE 	test.hexagonal_grid as hg SET total_time_spent = 
+		tot_edit_time
+		FROM grouped_time as gt
+		WHERE gt.nid = hg.nid ; 
+
