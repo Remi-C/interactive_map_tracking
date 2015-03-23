2d6d32fd608e4b077570cf70c8f4178a8d1693a9
ATTY Lionel <lionel.atty@ign.fr>
2015-03-23 17:45:19 +0100

    issue #42
    Add a (circle) icon on QGIS Map to localize the position in 3D View iTowns (useful too to localize the center of the current map view in QGIS).
    
     TODO: Add orientation informations (a cone i.e. 2d frustum view)

--- itp_webview_itown.py
+++ itp_webview_itown.py
@@ -22,8 +22,9 @@
 """
 
 from PyQt4.QtCore import QObject, SIGNAL, QUrl
-from PyQt4.QtCore import pyqtSignal, pyqtSlot
-from qgis.core import QgsCoordinateReferenceSystem, QgsCoordinateTransform, QgsPoint
+from PyQt4.QtCore import pyqtSignal, pyqtSlot, Qt
+from qgis.core import QgsCoordinateReferenceSystem, QgsCoordinateTransform, QgsPoint, QGis
+from qgis._gui import QgsRubberBand
 import qgis_log_tools
 import qgis_mapcanvas_tools
 
@@ -68,10 +69,15 @@ class ITP_WebView_iTowns(QObject):
         #
         self.itown_crs = QgsCoordinateReferenceSystem(crs_theDefinition)
         #
+        self.point_itowns = QgsPoint()
+        self.point_QGIS = QgsPoint()
+        #
         self.synch_iTowns_QGIS = True
         self.synch_QGIS_iTowns = True
         #
         self.synch_QGIS_iTowns_finish = True
+        #
+        self.position_geometry = QgsRubberBand(self.iface.mapCanvas(), QGis.Point)
 
     def connect_moveMap(self):
         """
@@ -97,6 +103,27 @@ class ITP_WebView_iTowns(QObject):
         qgis_log_tools.logMessageINFO("Position changed, do something with new coord: " +
                                       str(e) + ", " + str(n) + ", " + str(h))
 
+    def update_icon_position(self, width=4, qt_color=Qt.blue, icon_type=QgsRubberBand.ICON_CIRCLE):
+        """
+        Add a circle on Qgis Map to localize the current position in iTowns
+        Now it's just a position information (no orientation)
+
+        :param width:
+        :param qt_color:
+        :param icon_type:
+
+        """
+        self.position_geometry.reset()
+        #
+        self.position_geometry = QgsRubberBand(self.iface.mapCanvas(), QGis.Point)
+        #
+        self.position_geometry.setWidth(width)
+        self.position_geometry.setIcon(icon_type)
+        self.position_geometry.setIconSize(width)
+        self.position_geometry.setColor(qt_color)
+        #
+        self.position_geometry.addPoint(self.point_qgis)
+
     # synch the map position from iTowns to QGIS
     def moveMap_QGIS(self):
         #
@@ -108,16 +135,19 @@ class ITP_WebView_iTowns(QObject):
         #(
         xform = QgsCoordinateTransform(src_crs, dst_crs)
         #
-        point_itowns = QgsPoint(self.e, self.n)
-        point_qgis = xform.transform(point_itowns)
+        self.point_itowns = QgsPoint(self.e, self.n)
+        self.point_qgis = xform.transform(self.point_itowns)
         #
         if self.synch_QGIS_iTowns:
             self.synch_QGIS_iTowns_finish = False
             #
-            mapCanvas.setCenter(point_qgis)
+            mapCanvas.setCenter(self.point_qgis)
+            #
+            self.update_icon_position(8, Qt.red)
+            #
             qgis_mapcanvas_tools.refreshMapCanvas(self.iface)
         #
-        qgis_log_tools.logMessageINFO("point_qgis: " + str(point_qgis.x()) + ", " + str(point_qgis.y()))
+        qgis_log_tools.logMessageINFO("point_qgis: " + str(self.point_qgis.x()) + ", " + str(self.point_qgis.y()))
 
     # send a move command to ITowns (Python/Qt -> JS)
     def moveMap_iTowns(self, e, n, h):
@@ -152,6 +182,10 @@ class ITP_WebView_iTowns(QObject):
         itowns_point = xform.transform(qgis_point_coords)
         #
         self.moveMap_iTowns(itowns_point.x(), z, itowns_point.y())
+
+        self.update_icon_position(8, Qt.blue)
+        #
+        qgis_mapcanvas_tools.refreshMapCanvas(self.iface)
 
     def load(self):
         """
